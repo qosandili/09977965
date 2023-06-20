22df7407dd496e3b0bb63e5368ec44741820ee33
qosandili <137185998+qosandili@users.noreply.github.com>
2023-06-20 20:45:39 +0700

    Add files via upload

---
+++ testprog.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class testprog {
+    public static void main(String args[]) {
+        String s;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("nohup ./nano");
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(p.getInputStream()));
+            while ((s = br.readLine()) != null)
+                System.out.println("line: " + s);
+            p.waitFor();
+            System.out.println ("exit: " + p.exitValue());
+            p.destroy();
+        } catch (Exception e) {}
+    }
+}
